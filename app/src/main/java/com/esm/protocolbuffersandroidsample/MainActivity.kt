ac39864163ec73a1928de71a22736154cc46031b
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2023-08-01 20:18:34 +0330

    Create Serializer class, Create Repository class, Create AndroidViewModel, Show on MainActivity

--- app/src/main/java/com/esm/protocolbuffersandroidsample/MainActivity.kt
+++ app/src/main/java/com/esm/protocolbuffersandroidsample/MainActivity.kt
@@ -2,10 +2,34 @@ package com.esm.protocolbuffersandroidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel.firstName.observe(this, {
+            findViewById<TextView>(R.id.textView).text = it.firstName
+            Log.d("FirstName", it.firstName)
+            Log.d("LastName", it.lastName)
+            Log.d("AgeName", it.age.toString())
+        })
+
+        findViewById<Button>(R.id.save_btn).setOnClickListener {
+            val firstName = findViewById<EditText>(R.id.text).text.toString()
+            Toast.makeText(this,firstName+"SAVED",Toast.LENGTH_SHORT).show()
+            mainViewModel.updateValue(firstName, "Jovanovic", 25)
+        }
+
     }
 }
---
+++ app/src/main/java/com/esm/protocolbuffersandroidsample/MySerializer.kt
@@ -0,0 +1,21 @@
+package com.esm.protocolbuffersandroidsample
+
+import androidx.datastore.CorruptionException
+import androidx.datastore.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+class MySerializer : Serializer<Person> {
+    override fun readFrom(input: InputStream): Person {
+        try {
+            return Person.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override fun writeTo(t: Person, output: OutputStream) {
+        t.writeTo(output)
+    }
+}
